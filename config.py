27552fbfb037c0f771f5b5efc24138c8f6383772
jake-ds <godjsw1@gmail.com>
2020-09-14 19:43:43 +0900

    code refactoring <config.py>

--- config.py
+++ config.py
@@ -1,44 +1,55 @@
-
 config = {
+    "MODEL_DIR" : "Desktop/User/text_classifier_v2",
+    "data_fname" : {"train" : "./data/train/train.csv", 
+                    "validation" : "./data/validation/validation.csv", 
+                    "test" : "./data/test/test/csv"},
     
+    "dictionary_path" : "./dictionary/base_dictionary.txt",
+    "vocab_size" : 50000,
     
-    # data
-    "train_data" : ["train_data_A", "train_data_B"], 
-    "test_data" : "test_data",
-    "data_info" : "400.help/data_info.txt",
+    "label" : "label",
     
-    #label
-    "label" : "cateCode",
-    "cont_only" : False,
+    "contents_arg" : {"embedding_model" : "./help/word2vec_contents.model",
+                      "word_index" : "./help.tokenizer_contents.pickle",
+                      "pos_list" : ["NNP", "NNG", "NNB", "NR", "XPN", "NP", "VV", "MAG", "SL"],
+                      "MAX_NB_WORDS" : 50000,
+                      "EMBEDDING_DIM" : 100},
     
-    # pos
-    "pos_list" : ["NNP, NNG", "NNB", "NR", "NP", "VV", "MAG", "SL"],
+    "product_arg" : {"embedding_model" : "./help/word2vec_product.model",
+                      "word_index" : "./help.tokenizer_product.pickle",
+                      "MAX_NB_WORDS" : 50000,
+                      "EMBEDDING_DIM" : 100},
     
-    "voc_argument" : {
-        "embedding_model" : "400.help/voc_w2v_mdl.model",
-        "word_index" : "400.help/voc_tokenizer.pickle",
-        "dictionary" : "300.dictionary/voc_dictionary.txt",
-        "MAX_NB_WORDS" : 50000,
-        "EMBEDDING_DIM" : 100},
+    "padding_size" : {"title_pad_len" : 20, 
+                      "cont_pad_len" : 120,
+                      "product_pad_len" : 15},
     
-    "product_argument" : {
-        "embedding_model" : "400.help/product_w2v_mdl.model",
-        "word_index" : "400.help/product_tokenizer.pickle",
-        "dictionary" : "300.dictionary/product_dictionary.txt",
-        "MAX_NB_WORDS" : 50000,
-        "EMBEDDING_DIM" : 100},
+    "codeInfo_fname": {"M" : "./data/code_info/code_info_M.txt", 
+                       "S": "./data/code_info/code_info_S.txt"},
     
-    #padding
-    "title_padding" : 20, 
-    "cont_padding" : 100,
+    "model_path" : {"H-LSTM" : "./data/model/H-LSTM"}, 
     
-    "MAX_SEQUENCE_LENGTH" : 200,
-    "sampling_rate" : 0.2,
-    
-    #model
-    "epochs" : 500, 
-    "batch_size" : 1000}
+    "epochs" :100,
+    "batch_size" : 2000
+}
 
 
 def get():
     return config
+
+def show_config(config):
+    def dict_pring(dict_item):
+        print("{key} : {value}".format(key=dict_item[0], value=dict_item[1]))
+        
+    for item in config.items():
+        if type(item[1]) ==dict:
+            print("="*20)
+            print(item[0])
+            print("="*20)
+            
+            for item in item[1].items():
+                dict_print(item)
+            print("="*20)
+            
+        else:
+            dict_print(item)
